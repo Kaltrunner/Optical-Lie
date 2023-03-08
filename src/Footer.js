ac26a986df047bf1a5a218737a2811bb40e9fc1e
Erik Russell <erikrussells@gmail.com>
2023-03-08 12:37:05 -0500

    still need to add a next img button but ready to deploy

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
 
         <div className="line-div" ></div>
 
-            <p id="copy" >© Erik Russell <br/>thank you for visiting!</p>
+            <p id="copy" >© 2023 Erik Russell <br/>thank you for visiting!</p>
             <p id="copy" ></p>
            
         </div>
